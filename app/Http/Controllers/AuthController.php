f676e0adc703e905f2caf28241051562158ce162
billos-e <bill.ahognonvi@epitech.eu>
2025-10-16 10:58:21 +0100

    FEAT: config api and adding register routes and method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+
+        $credentials = $request->validate([
+            'name' => 'required|unique:users,name',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password'=> Hash::make($request->password)
+        ]);
+
+        // $token = $user->createToken('my-app-token');
+
+        return response()->json($user,201);
+
+    }
+}
