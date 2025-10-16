90ad8820e5e9105f0a516239975dd42809a3cc5e
billos-e <bill.ahognonvi@epitech.eu>
2025-10-16 14:28:31 +0100

    FIX: adding id field

--- database/migrations/2025_10_16_105230_create_user_modules_table.php
+++ database/migrations/2025_10_16_105230_create_user_modules_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_modules', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id');
             $table->foreignId('module_id');
             $table->boolean('active')->default(false);
