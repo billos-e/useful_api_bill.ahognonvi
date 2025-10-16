12e0e81424a48bf96466b6cb8cf9fff90dac4092
billos-e <bill.ahognonvi@epitech.eu>
2025-10-16 17:47:19 +0100

    FEAT: adding filable and hidden properties

--- app/Models/ShortLink.php
+++ app/Models/ShortLink.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class ShortLink extends Model
 {
     //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'original_url',
+        'code',
+        'clicks',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'updated_at',
+    ];
 }
