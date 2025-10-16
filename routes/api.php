fc995628d2e13b3cbcfc9613cc1deba99518492f
billos-e <bill.ahognonvi@epitech.eu>
2025-10-16 11:43:26 +0100

    UPDATE: removing unused middleware and routes

--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,11 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 // auth routes
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register')->name('register');
 
-    Route::middleware('api')->group(function () {
+    Route::post('login', 'login');
+    Route::post('logout', 'logout')->middleware('auth');;
 
-        Route::post('login', 'login');
-        Route::post('logout', 'logout');
-
-    });
 });
